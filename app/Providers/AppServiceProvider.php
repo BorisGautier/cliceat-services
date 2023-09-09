9f246fe65cb4807806321371328d74c445fc826e
TCHOUKOUAHA Boris Gautier <btchoukouaha@univ-catho-sjd.com>
2023-09-09 18:58:26 +0100

    update app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-         if ($this->app->environment('production')) {
+         if ($this->app->environment('live')) {
             URL::forceScheme('https');
         }
         try {

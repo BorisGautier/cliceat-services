8b6f1ecdd4c9a06c0f8134803a2008f65fcff862
TCHOUKOUAHA Boris Gautier <btchoukouaha@univ-catho-sjd.com>
2023-09-12 17:16:04 +0100

    update constants

--- app/CentralLogics/Constants.php
+++ app/CentralLogics/Constants.php
@@ -1,7 +1,7 @@
 <?php
 
 const APPS = [
-    'table_app' => [
+  /*  'table_app' => [
         'software_id' => 40488202,
         'app_name' => 'Efood table app',
         'buy_now_link'=>'https://codecanyon.net/item/efood-tablewaiter-app/40488202?s_rank=2'
@@ -10,5 +10,5 @@ const APPS = [
         'software_id' => 40488338,
         'app_name' => 'Efood kitchen app',
         'buy_now_link'=>'https://codecanyon.net/item/efood-kitchenchef-app/40488338?s_rank=1'
-    ],
+    ], */
 ];
